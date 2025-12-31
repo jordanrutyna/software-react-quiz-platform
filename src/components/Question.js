5f7779571e164a2fdfc37c827f7a77cc558abe10
Jordan Rutyna <217357884+jordanrutyna@users.noreply.github.com>
2025-12-31 12:33:12 -0500

    Display current question text and number

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,14 @@
 import Answer from './Answer'
+import { useContext } from 'react';
+import { QuizContext } from '../contexts/quiz';
 
 const Question = ({questions}) => { //arrow function is modern way of writing JS
-  console.log('Question', questions);
+  const [quizState] = useContext(QuizContext) // '{variable}' render variable in markup
+  const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
+  // console.log('Question', quizState);
   return (
     <div>
-      <div className='question'>Text of Question</div>
+      <div className='question'>{currentQuestion.question}</div>
       <div className='answers'>
         <Answer/>
         <Answer/>
