a70dc9faf27532acd00a7297e9bf587bca5e019f
Jordan Rutyna <217357884+jordanrutyna@users.noreply.github.com>
2026-01-07 13:48:19 -0500

    Async questions from Trivia API

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react'; // add useEffect for api 
 import Question from './Question'
 import { QuizContext } from '../contexts/quiz';
 
 const Quiz = () => {
   const [quizState, dispatch] = useContext(QuizContext);
+  const apiURL = "https://opentdb.com/api.php?amount=10&category=19&difficulty=easy&type=multiple&encode=url3986"
   // console.log('quizState', quizState);
+  
+  useEffect(() => { // add for api 
+    if (quizState.questions.length > 0) { // add check for restart button to work
+      return;
+    }
+    console.log('on initialize');
+
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(data => {
+        console.log("data", data);
+        dispatch({ type: "LOADED_QUESTIONS", payload: data.results });
+      });
+  }); // change from }, []); for restart button to work (removed empty array)
+
   return (
     <div className='quiz'>
       {quizState.showResults && (
@@ -17,7 +33,7 @@ const Quiz = () => {
           <div className='next-button' onClick={() => dispatch({ type: "RESTART"})}>Restart</div>
         </div>
       )}
-      {!quizState.showResults && (
+      {!quizState.showResults && quizState.questions.length > 0 && ( /* added for api quizState.questions.length > 0 && */
         <div>
           <div className='score'>Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}</div>
           <Question />
--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,12 +1,12 @@
 import { createContext, useReducer } from "react";
-import questions from "../data";
-import { shuffleAnswers } from "../helpers";
+// import questions from "../data"; // remove for api
+import { normalizeQuestions, shuffleAnswers } from "../helpers";
 
 const initialState = {
   currentQuestionIndex: 0,
-  questions,                // short form for 'questions: questions'
+  questions: [],       // changed for api from 'questions,' (short form 'questions: questions')
   showResults: false,
-  answers: shuffleAnswers(questions[0]),
+  answers: [],         // changed for api from 'answers: shuffleAnswers(questions[0]),'
   currentAnswer: '',
   correctAnswersCount: 0,
 }
@@ -46,36 +46,24 @@ const reducer = (state, action) => {
     case "RESTART": {
       return initialState;
     }
+    case "LOADED_QUESTIONS": {
+      console.log('LOADED_QUESTIONS', state, action);
+      const normalizedQuestions = normalizeQuestions(action.payload);
+      return {
+        ...state,
+        questions: normalizedQuestions,
+        answers: shuffleAnswers(normalizedQuestions[0]),
+      }
+    }
     default: {
       return state;
     }
   }
-
-  // if (action.type === 'NEXT_QUESTION') {
-  //   const showResults = state.currentQuestionIndex === state.questions.length - 1;
-  //   const currentQuestionIndex = showResults 
-  //     ? state.currentQuestionIndex 
-  //     : state.currentQuestionIndex + 1;
-  //   const answers = showResults 
-  //     ? [] 
-  //     : shuffleAnswers(state.questions[currentQuestionIndex]);
-  //   return {
-  //     ...state, 
-  //     currentQuestionIndex: state.currentQuestionIndex + 1, 
-  //     showResults,
-  //     answers,
-  //   };
-  // }
-  // if (action.type === 'RESTART') {
-  //   return initialState;
-  // }
-  // return state;
 }
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  // const [state, dispatch] = useReducer(reducer, initialState);
   const value = useReducer(reducer, initialState);
   // console.log("state", value)
   return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
